a4c3b44b05deead557b43c6391fc5d5cc5cab5da
Eissanoor <eissaanoor@gmail.com>
2024-09-25 22:42:56 +0500

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,7 @@ dotenv.config({ path: "./config.env" });
 const { cloudinary } = require("../config/cloudanary.js");
 const C_cloud_name = process.env.C_cloud_name
 require('../auth/auth.js');
-const frontendbaseURL = "http://localhost:592"
+const frontendbaseURL = "https://pakardi.com"
 const addUser = async (req, res) => {
   const { username, email, password, dateOfBirth, aboutMe, phone, address } = req.body;
   const imagePath = req.files?.image ? `/uploads/${req.files.image[0].filename}` : null;
