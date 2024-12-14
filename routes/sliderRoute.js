851ce0ec443d6685fff34a58636addf893b1b582
Eissanoor <eissaanoor@gmail.com>
2024-12-14 11:55:57 +0500

    updated

--- routes/sliderRoute.js
+++ routes/sliderRoute.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+const apicache = require('apicache');
+const cache = apicache.middleware;
 const sliderController = require('../controllers/sliderController');
 const multer = require('multer');
 const { upload } = require("../config/multerConfig");
 
 // const upload = multer({ storage: storage });
 
-router.get('/', sliderController.getallSlider);
+router.get('/', cache('5 minutes'), sliderController.getallSlider);
 router.get("/:id", sliderController.getSliderById)
 router.post('/', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'icon', maxCount: 1 }]), sliderController.addnewSlider);
 router.put('/:id', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'icon', maxCount: 1 }]), sliderController.updateSlider);
